201818948f9122f27a3ef1aea5865d2fd986b87f
Aaqib Mehran <mehran.aaqib@gmail.com>
2021-04-25 02:57:24 +0500

    Packages are updated and Bootstrap-Email is added

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,9 @@ const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 const notify = require('gulp-notify');
 const injectPartials = require('gulp-inject-partials');
+const gulp_htmlmin = require('gulp-htmlmin');
+const gulp_inline_images = require('gulp-inline-images');
+const gulp_bootstrap_email = require('gulp-bootstrap-email');
 
 /*
 	--Top level functions--
@@ -33,33 +36,44 @@ const injectPartials = require('gulp-inject-partials');
 
 // Copy html files
 gulp.task('copyHtml', function(){
-	gulp.src('source/*.html')
+	return gulp.src('source/*.html')
 	.pipe(injectPartials({
 		removeTags: true
 	}))
 	.pipe(gulp.dest('app'));
 });
 
+gulp.task('email', () => {
+	return gulp.src(['source/emails/*', 'source/emails/**/*'])
+		.pipe(gulp_bootstrap_email())
+		.pipe(gulp_htmlmin({
+			removeComments: true,
+			collapseWhitespace: true
+		}))
+		.pipe(gulp_inline_images())
+		.pipe(gulp.dest('app/emails'))
+});
+
 // Optimize Images
 gulp.task('imageMin', function(){
-    gulp.src(['source/images/*', 'source/images/**/*'])
+	return gulp.src(['source/images/*', 'source/images/**/*'])
         .pipe(imagemin())
         .pipe(gulp.dest('app/images'))
 });
 // Copy Videos
 gulp.task('copyVid', function(){
-	gulp.src(['source/videos/*', 'source/videos/**/*'])
+	return gulp.src(['source/videos/*', 'source/videos/**/*'])
 		.pipe(gulp.dest('app/videos'))
 });
 // Copy Fonts
 gulp.task('copyFont', function(){
-	gulp.src(['source/fonts/*', 'source/fonts/**/*'])
+	return gulp.src(['source/fonts/*', 'source/fonts/**/*'])
 		.pipe(gulp.dest('app/fonts'))
 });
 
 //Minify JS
 gulp.task('scripts', function(){
-	gulp.src('source/js/*.js')
+	return gulp.src('source/js/*.js')
 	.pipe(concat('main.js'))
 	.pipe(uglify())
 	.pipe(gulp.dest('app/js'))
@@ -70,7 +84,7 @@ gulp.task('scripts', function(){
 
 //Compile SCSS
 gulp.task('scss', function(){
-	gulp.src('source/scss/styles.scss')
+	return gulp.src('source/scss/styles.scss')
 	.pipe(sass()).on('error', sass.logError)
 	.pipe(gulp.dest('app/css'))
 	.pipe(browserSync.stream())
@@ -85,12 +99,13 @@ gulp.task('serve', function(){
 		server: "./app"
 	});
 
-	gulp.watch(['source/js/*.js', 'source/js/**/*.js'], ['scripts']);
-	gulp.watch(['source/images/*', 'source/images/**/*'], ['imageMin']);
-	gulp.watch(['source/videos/*', 'source/videos/**/*'], ['copyVid']);
-	gulp.watch(['source/fonts/*', 'source/fonts/**/*'], ['copyFont']);
-    gulp.watch(['source/scss/*.scss', 'source/scss/**/*.scss'], ['scss']);
-    gulp.watch(['source/*.html', 'source/partials/*.html'], ['copyHtml']).on('change', browserSync.reload);
+	gulp.watch(['source/js/*.js', 'source/js/**/*.js'], gulp.series(['scripts']));
+	gulp.watch(['source/images/*', 'source/images/**/*'], gulp.series(['imageMin']));
+	gulp.watch(['source/videos/*', 'source/videos/**/*'], gulp.series(['copyVid']));
+	gulp.watch(['source/fonts/*', 'source/fonts/**/*'], gulp.series(['copyFont']));
+	gulp.watch(['source/scss/*.scss', 'source/scss/**/*.scss'], gulp.series(['scss']));
+	gulp.watch(['source/*.html', 'source/partials/*.html'], gulp.series(['copyHtml'])).on('change', browserSync.reload);
+	gulp.watch(['source/emails/*.html', 'source/emails/**/*.html'], gulp.series(['email'])).on('change', browserSync.reload);
 });
 
-gulp.task('default', ['serve', 'copyHtml', 'imageMin', 'copyVid', 'copyFont', 'scripts']);
+gulp.task('default', gulp.series(['serve', 'copyHtml', 'imageMin', 'copyVid', 'copyFont', 'scripts', 'email']));
